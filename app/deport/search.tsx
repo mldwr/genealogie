1f9766e4ced952d03e585ff8de13ce72d9b28501
Waldemar Hujo <waldemarhujo@gmail.com>
2025-01-05 15:58:42 +0100

    add clear search functionality to search component

--- app/deport/search.tsx
+++ app/deport/search.tsx
@@ -1,15 +1,15 @@
 'use client';
 
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { MagnifyingGlassIcon, XCircleIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
+import { useState } from 'react';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  //function handleSearch(term: string) {
   const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
@@ -20,7 +20,14 @@ export default function Search({ placeholder }: { placeholder: string }) {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }, 600)
+  }, 600);
+
+  const [searchTerm, setSearchTerm] = useState(searchParams.get('query')?.toString() || '');
+
+  const clearSearch = () => {
+    setSearchTerm('');
+    handleSearch('');
+  };
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -29,14 +36,21 @@ export default function Search({ placeholder }: { placeholder: string }) {
       </label>
       <input
         id='search'
-        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 pr-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        value={searchTerm}
         onChange={(e) => {
+          setSearchTerm(e.target.value);
           handleSearch(e.target.value);
         }}
-        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+      {searchTerm && (
+        <XCircleIcon
+          className="absolute right-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 cursor-pointer"
+          onClick={clearSearch}
+        />
+      )}
     </div>
   );
 }
