d51949a340a6e4e2e2ea648bb39c0fac155fbd8f
Waldemar Hujo <waldemarhujo@gmail.com>
2025-01-12 21:24:21 +0100

    fix(header): adjust spacing for responsive design in header component

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -25,7 +25,7 @@ export default function Header({ user }: NavlinksProps) {
     {!hideHeaderRoutes.includes(pathname) && 
       <header className="fixed top-2 z-30 w-full lg:top-6">
         <div className="mx-auto max-w-6xl px-4 sm:px-6">
-          <div className="relative flex h-14 items-center justify-between gap-3 rounded-2xl bg-white/90 px-3 shadow-lg shadow-black/[0.03] backdrop-blur-sm before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(theme(colors.gray.100),theme(colors.gray.200))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)]">
+          <div className="relative flex h-14 items-center justify-between sm:gap-3 rounded-2xl bg-white/90 sm:px-3 shadow-lg shadow-black/[0.03] backdrop-blur-sm before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(theme(colors.gray.100),theme(colors.gray.200))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)]">
             {/* Site branding */}
             <div className="flex flex-1 items-center">
               <Logo />
