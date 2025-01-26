1e6179b2f4425734120cda0a95da9f826bdf7fd2
Waldemar Hujo <waldemarhujo@gmail.com>
2025-01-26 12:57:58 +0100

    feat(participate): enhance styling and layout for participate page

--- app/participate/page.tsx
+++ app/participate/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ParticipatePage: React.FC = () => {
     return (
-        <div className="flex justify-center items-center min-h-screen mt-28">
+        <div className="font-sans p-5 max-w-xl mx-auto bg-gray-100 rounded-lg shadow-lg mt-28">
             <div className="flex flex-col p-4">
                 <h1 className="text-4xl font-bold mb-8">Teilnahme am Projekt</h1>
                 <section className="mb-8">
