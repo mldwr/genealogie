114c31c761e22606e218cccd46b43371269ddb63
Waldemar Hujo <waldemarhujo@gmail.com>
2025-11-24 19:59:01 +0100

    fix: simplify expression

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,8 +3,9 @@ import { createClient } from '@/utils/supabase/server';
 import { redirect } from 'next/navigation';
 
 export default async function Page() {
-  const supabase = createClient();
-  const { data: { user } } = await (await supabase).auth.getUser();
+  const supabase = await createClient();
+  const { data } = await supabase.auth.getUser();
+  const user = data.user;
 
   if (!user) {
     return redirect('/signin');
@@ -13,7 +14,7 @@ export default async function Page() {
   const deportedData = await fetchDeported('', 1); // Fetch all data for now
 
   return (
-    <div className="container mx-auto p-4">
+    <div className="w-full mt-14">
       <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         <div className="bg-white p-4 rounded-lg shadow">
